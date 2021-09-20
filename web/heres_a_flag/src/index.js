32cbaf27de2811e7814eaa99f6fb999449549735
Krish Chatterjie <chatterjie.krish@gmail.com>
2021-09-20 14:26:57 +0530

    add-HeresAFlag-easy-web

---
+++ web/heres_a_flag/src/index.js
@@ -0,0 +1,3 @@
+// aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g_dj1kUXc0dzlXZ1hjUQ
+// since this a warmup, i'll let you know that this is base64 encoded
+// and you should check the yt comments of the decoded link
